bce14f4eb607900e751797cc0bb8c11228caa469
Kutay Malik Tatas <tatasmalik@gmail.com>
2023-10-17 11:24:12 +0300

    'hw3'

---
+++ SQL HW3.sql
@@ -0,0 +1,13 @@
+-- SELECT * FROM customer WHERE first_name LIKE 'D%n';
+-- SELECT * FROM customer WHERE first_name ILIKE 'a%';
+-- SELECT * FROM customer WHERE first_name NOT LIKE '%A';
+-- SELECT * FROM customer WHERE first_name LIKE 'J_an';
+-- SELECT * FROM customer WHERE first_name ~~ 'J_an';
+-- SELECT * FROM customer WHERE first_name ~~* 'b%';
+-- SELECT * FROM customer WHERE first_name !~~ 'B%';
+
+-- Homework 3
+-- SELECT * FROM country WHERE country LIKE 'A%a';
+-- SELECT * FROM country WHERE country LIKE '%_____n';
+-- SELECT * FROM film WHERE LENGTH(title) >=4 AND title ILIKE '%T%';
+-- * FROM film WHERE title LIKE 'C%' AND length >= 90;
